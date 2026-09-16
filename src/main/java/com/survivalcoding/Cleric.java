c8051b6d997c0eedbfc02dbd4c7e586255d17471
Namjun Jang <tkfkddlgkxmm@gmail.com>
2026-09-16 15:39:43 +0900

    3번째 까지 했는데 이거 식 틀렸을수도 있음 점검필요

--- src/main/java/com/survivalcoding/Cleric.java
+++ src/main/java/com/survivalcoding/Cleric.java
@@ -1,19 +1,36 @@
 package com.survivalcoding;
 
+import java.util.Random;
+
 public class Cleric {
     String name;
     int hp;
     int mp;
     final int MAXHP = 50;
     final int MAXMP =10;
+    int oldmp = this.mp;
 
     void selfAid() {
         this.mp -= 5;
         this.hp += MAXHP;
-        System.out.println("스킬 사용 selfaid!!");
+        System.out.println("스킬 사용 self aid!!");
+    }
+    int pray(int sec) {
+        Random random = new Random();
+        int bonus = random.nextInt(3);
+        int prayTime = sec + bonus;
+        int prayDo = prayTime + mp;
+        if (prayDo > MAXMP) {
+            prayDo = MAXMP;
+            System.out.println("이미 마나가 최대입니다.");
+        }
+        this.mp = prayDo;
+        return prayDo - oldmp;
+
     }
 
-    
+
+
 
 
 
