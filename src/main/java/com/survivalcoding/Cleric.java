5b90fdeeac65a8e62a3e3b0a5493b4d8103fe7b5
Namjun Jang <tkfkddlgkxmm@gmail.com>
2026-09-16 14:19:46 +0900

    Cleric 연습문제2 번까지

--- src/main/java/com/survivalcoding/Cleric.java
+++ src/main/java/com/survivalcoding/Cleric.java
@@ -7,5 +7,14 @@ public class Cleric {
     final int MAXHP = 50;
     final int MAXMP =10;
 
+    void selfAid() {
+        this.mp -= 5;
+        this.hp += MAXHP;
+        System.out.println("스킬 사용 selfaid!!");
+    }
+
+    
+
+
 
 }
