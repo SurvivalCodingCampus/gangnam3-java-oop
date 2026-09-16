9384522f13da5323d102ded605b16e1e55cf2178
gghwan <gghwan93@gmail.com>
2026-09-16 22:52:46 +0900

    [2026-09-16] 김경환 - 과제 제출

--- src/main/java/com/survivalcoding/Main.java
+++ src/main/java/com/survivalcoding/Main.java
@@ -8,8 +8,6 @@ public class Main {
     public static void main(String[] args) {
         int i = 10;
         String name = "홍길동";
-
         Hero hero = new Hero();
-        Hero hero2 = new Hero();
     }
 }
