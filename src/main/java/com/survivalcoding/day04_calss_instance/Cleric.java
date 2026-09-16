7dcd8beb4d316ae76b7006bbe6f94f0fd08ddd7a
kamie <k.me4dev@gmail.com>
2026-09-16 14:51:07 +0900

    feat: 성직자 selfAid 마법 메서드 ì추가

--- src/main/java/com/survivalcoding/day04_calss_instance/Cleric.java
+++ src/main/java/com/survivalcoding/day04_calss_instance/Cleric.java
@@ -7,4 +7,13 @@ public class Cleric {
     
     int maxHp = 50;
     int maxMp = 10;
+    
+    int mp4selfAid = 5;
+    
+    void selfAid() {  // 셀프 에이드 마법 사용
+        if ((this.mp - this.mp4selfAid) >= 0) {
+            this.mp -= this.mp4selfAid;
+            this.hp = this.maxHp;
+        }
+    }
 }
