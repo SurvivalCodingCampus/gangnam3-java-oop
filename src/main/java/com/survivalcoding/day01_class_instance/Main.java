971a3695124623e52ab1246c552ee78485f64129
Junsuk Oh <junsuk.oh@survivalcoding.com>
2026-09-17 15:47:06 +0900

    feat: 클래스

--- src/main/java/com/survivalcoding/day01_class_instance/Main.java
+++ src/main/java/com/survivalcoding/day01_class_instance/Main.java
@@ -1,6 +1,4 @@
-package com.survivalcoding;
-
-import com.survivalcoding.day01_class_instance.Hero;
+package com.survivalcoding.day01_class_instance;
 
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
@@ -9,7 +7,14 @@ public class Main {
         int i = 10;
         String name = "홍길동";
 
+        int min = Math.min(10, 5);
+
         Hero hero = new Hero();
-        Hero hero2 = new Hero();
+        hero.name = "오준석";
+
+        System.out.println(hero.hp);
+
+        Hero hero2 = new Hero("오준석");
+        Hero hero3 = new Hero(100);
     }
 }
