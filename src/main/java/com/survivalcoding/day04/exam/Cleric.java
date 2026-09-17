5d5e6dec7ff5baf0dd87967504625efc8e997c34
SiHyeong Eom <luna17f@naver.com>
2026-09-17 15:09:03 +0900

    feat: day04 과제 가독성 및 컨벤션 수정

--- src/main/java/com/survivalcoding/day04/exam/Cleric.java
+++ src/main/java/com/survivalcoding/day04/exam/Cleric.java
@@ -3,25 +3,22 @@ package com.survivalcoding.day04.exam;
 import java.util.Random;
 
 public class Cleric {
+    final Random random = new Random();
+    final int costForSelfAid = 5;
+    final int maxCorrectionValue = 2;
+    final int maxHp = 50;
+    final int maxMp = 10;
 
-    final Random RANDOM = new Random();
-    final int COST_FOR_SELF_AID = 5;
-    final int MAX_CORRECTION_VALUE = 2;
-    int RANDOM_CORRECTION_VALUE;
-
-    int MAX_HP = 50;
-    int MAX_MP = 10;
-
-    int hp = MAX_HP;
-    int mp = MAX_MP;
+    int hp = maxHp;
+    int mp = maxMp;
 
     void selfAid() {
-        if (mp - COST_FOR_SELF_AID < 0) {
+        if (mp - costForSelfAid < 0) {
             System.out.println("마나가 부족합니다");
         } else {
-            mp -= COST_FOR_SELF_AID;
-            hp = MAX_HP;
-            System.out.println("MP " + COST_FOR_SELF_AID + "를 소비하여 MAX HP로 회복했습니다");
+            mp -= costForSelfAid;
+            hp = maxHp;
+            System.out.println("MP " + costForSelfAid + "를 소비하여 MAX HP로 회복했습니다");
         }
     }
 
@@ -29,13 +26,12 @@ public class Cleric {
      * 캐릭터의 MP를 회복시킵니다.
      *
      * @param durationSecond 기도 시간
-     * @return 정상 처리 시 회복량, <br>
-     *         이미 최대값인 경우 0, <br>
+     * @return 정상 처리 시 회복량 <br>
+     *         이미 최대값인 경우 0 <br>
      *         범위를 벗어난 경우 -1
      */
     int pray(int durationSecond) {
-
-        if (mp == MAX_MP) {
+        if (mp == maxMp) {
             System.out.println("이미 최대 마나입니다");
             return 0;
         } else if (durationSecond <= 0) {
@@ -43,12 +39,12 @@ public class Cleric {
             return -1;
         }
 
-        RANDOM_CORRECTION_VALUE = RANDOM.nextInt(MAX_CORRECTION_VALUE + 1);
+        // 보정치는 0 ~ 2까지지만 nextInt 특성상 + 1
+        final int randomCorrectionValue = random.nextInt(maxCorrectionValue + 1);
+        int mpRestoreAmount = durationSecond + randomCorrectionValue;
 
-        int mpRestoreAmount = durationSecond + RANDOM_CORRECTION_VALUE;
-
-        if (MAX_MP < mp + mpRestoreAmount) {
-            mpRestoreAmount = MAX_MP - mp;
+        if (maxMp < mp + mpRestoreAmount) {
+            mpRestoreAmount = maxMp - mp;
         }
 
         mp += mpRestoreAmount;
--- src/test/java/com/survivalcoding/day04/exam/ClericTest.java
+++ src/test/java/com/survivalcoding/day04/exam/ClericTest.java
@@ -3,123 +3,125 @@ package com.survivalcoding.day04.exam;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
-import java.util.Random;
-
 import static org.junit.jupiter.api.Assertions.*;
 
 class ClericTest {
+
     @Test
     @DisplayName("heal을 하면 hp를 10 회복해야 한다")
     void heal() {
         // Given
-        final Cleric CLERIC = new Cleric();
-        CLERIC.mp = 6;
+        final Cleric cleric = new Cleric();
 
-        final int BEFORE_MP = CLERIC.mp;
+        // mp소비 코스트보다 높은 값
+        final int enoughMp = cleric.costForSelfAid;
+        cleric.mp = enoughMp;
+        final int beforeMp = cleric.mp;
 
         // When
-        CLERIC.selfAid();
+        cleric.selfAid();
 
         // Then
-        assertEquals(CLERIC.MAX_HP, CLERIC.hp);
-        assertEquals(BEFORE_MP - CLERIC.COST_FOR_SELF_AID, CLERIC.mp);
+        assertEquals(cleric.maxHp, cleric.hp);
+        assertEquals(beforeMp - cleric.costForSelfAid, cleric.mp);
     }
 
     @Test
     @DisplayName("MP가 부족하면 회복을 못 한다")
     void heal2() {
         // Given
-        final Cleric CLERIC = new Cleric();
-        CLERIC.mp = 4;
+        final Cleric cleric = new Cleric();
 
-        final int BEFORE_HP = CLERIC.hp;
-        final int BEFORE_MP = CLERIC.mp;
+        // 스킬 사용 불가한 costForSelfAid - 1 로 설정
+        final int notEnoughMp = cleric.costForSelfAid - 1;
+        cleric.mp = notEnoughMp;
+        final int beforeHp = cleric.hp;
+        final int beforeMp = cleric.mp;
 
         // When
-        CLERIC.selfAid();
+        cleric.selfAid();
 
         // Then
-        assertEquals(BEFORE_HP, CLERIC.hp);
-        assertEquals(BEFORE_MP, CLERIC.mp);
+        assertEquals(beforeHp, cleric.hp);
+        assertEquals(beforeMp, cleric.mp);
     }
 
     @Test
     @DisplayName("회복 마나가 최댓값보다 작을 때")
     void restoreMp() {
         // Given
-        final Cleric CLERIC = new Cleric();
-        CLERIC.mp = 1;
+        final Cleric cleric = new Cleric();
 
-        final int BEFORE_MP = CLERIC.mp;
-        final int DURATION_SECOND = 9;
+        // 최대값보다 작은 값
+        final int notMaxMp = cleric.maxMp - 1;
+        cleric.mp = notMaxMp;
+        final int beforeMp = cleric.mp;
+        final int durationSecond = 9;
 
-        final int MIN_MP_RESTORE_AMOUNT = BEFORE_MP + DURATION_SECOND;
-        final int MAX_MP_RESTORE_AMOUNT = MIN_MP_RESTORE_AMOUNT + CLERIC.MAX_CORRECTION_VALUE;
+        final int minMpRestoreAmount = beforeMp + durationSecond;
+        final int maxMpRestoreAmount = minMpRestoreAmount + cleric.maxCorrectionValue;
 
         // When
-        final int MP_RESTORE_AMOUNT = CLERIC.pray(DURATION_SECOND);
-
-        boolean isInRange = MIN_MP_RESTORE_AMOUNT <= CLERIC.mp && CLERIC.mp <= MAX_MP_RESTORE_AMOUNT;
+        final int mpRestoreAmount = cleric.pray(durationSecond);
+        boolean isInRange = minMpRestoreAmount <= cleric.mp && cleric.mp <= maxMpRestoreAmount;
 
         // Then
         assertTrue(isInRange);
-        assertEquals(BEFORE_MP + MP_RESTORE_AMOUNT, CLERIC.mp);
+        assertEquals(beforeMp + mpRestoreAmount, cleric.mp);
     }
 
     @Test
     @DisplayName("회복 마나가 maxMp를 넘길 때")
-    void restoreMp11() {
+    void restoreMp2() {
         // Given
-        final Cleric CLERIC = new Cleric();
-        CLERIC.mp = 8;
+        final Cleric cleric = new Cleric();
 
-        final int BEFORE_MP = CLERIC.mp;
-        final int DURATION_SECOND = 3;
+        // mp 최대값에 가깝게
+        final int mpNearMax = cleric.maxMp - 1;
+        cleric.mp = mpNearMax;
+        final int beforeMp = cleric.mp;
+        final int durationSecond = 3;
 
         // When
-        final int MP_RESTORE_AMOUNT = CLERIC.pray(DURATION_SECOND);
+        final int mpRestoreAmount = cleric.pray(durationSecond);
 
         // Then
-        assertEquals(CLERIC.MAX_MP, BEFORE_MP + MP_RESTORE_AMOUNT);
-        assertEquals(CLERIC.MAX_MP, CLERIC.mp);
+        assertEquals(cleric.maxMp, beforeMp + mpRestoreAmount);
+        assertEquals(cleric.maxMp, cleric.mp);
     }
 
     @Test
     @DisplayName("잘못된 기도 시간")
-    void restoreMp2() {
+    void restoreMp3() {
         // Given
-        final Cleric CLERIC = new Cleric();
-        CLERIC.mp = 0;
-
-        final int DURATION_SECOND = -1;
-        final int INVALID_DURATION_ERR_CODE = -1;
-        final int BEFORE_MP = CLERIC.mp;
+        final Cleric cleric = new Cleric();
+        final int invalidDurationSecond = -1;
+        final int invalidDurationErrCode = -1;
+        final int beforeMp = cleric.mp;
 
         // When
-        final int MP_RESTORE_AMOUNT = CLERIC.pray(DURATION_SECOND);
+        final int mpRestoreAmount = cleric.pray(invalidDurationSecond);
 
         // Then
-        assertEquals(INVALID_DURATION_ERR_CODE, MP_RESTORE_AMOUNT);
-        assertEquals(BEFORE_MP, CLERIC.mp);
+        assertEquals(invalidDurationErrCode, mpRestoreAmount);
+        assertEquals(beforeMp, cleric.mp);
     }
 
     @Test
     @DisplayName("최대 마나면 회복 불가")
-    void restoreMp3() {
+    void restoreMp4() {
         // Given
-        final Cleric CLERIC = new Cleric();
-        CLERIC.mp = 10;
-
+        final Cleric cleric = new Cleric();
+        cleric.mp = cleric.maxMp;
         final int durationSecond = 5;
-        final int MAX_MP_CODE = 0;
-        final int BEFORE_MP = CLERIC.mp;
+        final int maxMpCode = 0;
+        final int beforeMp = cleric.mp;
 
         // When
-        final int MP_RESTORE_AMOUNT = CLERIC.pray(durationSecond);
-
+        final int mpRestoreAmount = cleric.pray(durationSecond);
 
         // Then
-        assertEquals(MAX_MP_CODE, MP_RESTORE_AMOUNT);
-        assertEquals(BEFORE_MP, CLERIC.mp);
+        assertEquals(maxMpCode, mpRestoreAmount);
+        assertEquals(beforeMp, cleric.mp);
     }
 }
