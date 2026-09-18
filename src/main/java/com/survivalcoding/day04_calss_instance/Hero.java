9524fb5a034cb3a10bf6383b10b7a17afac7ed58
kamie <k.me4dev@gmail.com>
2026-09-18 15:56:38 +0900

    feat: Hero: 함수 bye, die 추가

--- src/main/java/com/survivalcoding/day04_calss_instance/Hero.java
+++ src/main/java/com/survivalcoding/day04_calss_instance/Hero.java
@@ -6,8 +6,9 @@ public class Hero {
     // 필드(field), 멤버변수(member variable), 속성(property), 전역변수,
     static int money = 100;
     
+    private int hp;
+    
     String name;
-    int hp;
     Sword sword;
     
     // 생성자
@@ -57,5 +58,13 @@ public class Hero {
     void sleep() {
         this.hp = 100;  // 100 = magic number. 이게 뭔데? 라고 물어볼 수 있음. max_hp 이런 식으로 따로 설정해줘야 함
         System.out.printf("%s는 잠을 자고 회복했다!%n", this.name);
+    }
+    
+    public void bye() {
+        System.out.println("빠이");
+    }
+    
+    private void die() {
+        System.out.printf("%s가 죽었다%n", this.name);
     }
 }
