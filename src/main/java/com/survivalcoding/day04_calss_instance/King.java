4252f8b25e11a68fa7de496b5caecaac6aea3470
kamie <k.me4dev@gmail.com>
2026-09-18 15:52:28 +0900

    feat: King: class King 추가

---
+++ src/main/java/com/survivalcoding/day04_calss_instance/King.java
@@ -0,0 +1,9 @@
+package com.survivalcoding.day04_calss_instance;
+
+public class King {
+    void talk(Hero hero) {
+        System.out.printf("왕 : 우리 성에 어서 오시오 용사 %n이여", hero.name);
+        System.out.println("왕 : 긴 여행에 피로하겠군");
+        System.out.println("왕 : 우선 성 아랫 마을을 보고 와도 좋소. 그럼 또 봅시다");
+    }
+}
