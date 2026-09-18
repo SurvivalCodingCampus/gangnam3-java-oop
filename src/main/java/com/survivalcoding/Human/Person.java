717a4dfd72126fcdde1c6feddcf1a11df28fd2b1
kamie <k.me4dev@gmail.com>
2026-09-18 20:56:59 +0900

    feat: Person: 이름과 출생연도 값에 대한 타당성 검사

--- src/main/java/com/survivalcoding/Human/Person.java
+++ src/main/java/com/survivalcoding/Human/Person.java
@@ -1,10 +1,22 @@
 package com.survivalcoding.Human;
 
+import com.survivalcoding.day04_calss_instance.Validator;
+
+import java.time.LocalDate;
+
 public class Person {
+    static final int MIN_NAME_LENGTH = 1;
+    
+    private int thisYear;
+    
     private final String name;
     private final int birthYear;
     
     Person(String name, int birthYear) {
+        Validator.validateNotNull(name);
+        Validator.validateMinLength(name, MIN_NAME_LENGTH);
+        Validator.validateAtMost(birthYear, thisYear);
+        
         this.name = name;
         this.birthYear = birthYear;
     }
