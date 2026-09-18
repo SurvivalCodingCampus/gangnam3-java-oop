98a781d264226c9ec6f681e2aeecb38de223ab85
kamie <k.me4dev@gmail.com>
2026-09-18 15:38:57 +0900

    feat: Wizard: class Wizard 생성

---
+++ src/main/java/com/survivalcoding/day04_calss_instance/Wizard.java
@@ -0,0 +1,11 @@
+package com.survivalcoding.day04_calss_instance;
+
+public class Wizard {
+    String name;
+    int hp;
+    
+    void heal(Hero hero) {
+        hero.hp += 10;
+        System.out.printf("%s의 HP 10을 회복했다!%n", hero.name);
+    }
+}
