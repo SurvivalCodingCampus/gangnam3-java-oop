aa11273f6a672f54eda7419cebf80634b0bd3908
kamie <k.me4dev@gmail.com>
2026-09-18 20:51:19 +0900

    feat: Person: class Person 및 생성자 구현

---
+++ src/main/java/com/survivalcoding/Human/Person.java
@@ -0,0 +1,11 @@
+package com.survivalcoding.Human;
+
+public class Person {
+    private final String name;
+    private final int birthYear;
+    
+    Person(String name, int birthYear) {
+        this.name = name;
+        this.birthYear = birthYear;
+    }
+}
