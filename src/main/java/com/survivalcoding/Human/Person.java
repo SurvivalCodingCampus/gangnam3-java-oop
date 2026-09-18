099a619fb58b02ac09f2db4a37e17bac8d4fba6b
kamie <k.me4dev@gmail.com>
2026-09-18 21:04:32 +0900

    feat: Person: getter 생성: name, birthYear, thisYear, Age

--- src/main/java/com/survivalcoding/Human/Person.java
+++ src/main/java/com/survivalcoding/Human/Person.java
@@ -6,8 +6,7 @@ import java.time.LocalDate;
 
 public class Person {
     static final int MIN_NAME_LENGTH = 1;
-    
-    private int thisYear = getThisYear();
+    static final int MIN_BIRTH_YEAR = 1800;
     
     private final String name;
     private final int birthYear;
@@ -15,13 +14,26 @@ public class Person {
     Person(String name, int birthYear) {
         Validator.validateNotNull(name);
         Validator.validateMinLength(name, MIN_NAME_LENGTH);
-        Validator.validateAtMost(birthYear, thisYear);
+//        Validator.validateAtLeast(birthYear, MIN_BIRTH_YEAR);
+        Validator.validateAtMost(birthYear, getThisYear());
         
         this.name = name;
         this.birthYear = birthYear;
     }
     
+    public String getName() {
+        return name;
+    }
+    
+    public int getBirthYear() {
+        return birthYear;
+    }
+    
     private int getThisYear() {
         return LocalDate.now().getYear();
+    }
+    
+    private int getAge() {
+        return getThisYear() - birthYear;
     }
 }
