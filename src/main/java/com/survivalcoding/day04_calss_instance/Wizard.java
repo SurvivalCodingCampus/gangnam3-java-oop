d244466d2a985ea0d52ccc47fa7c8ff742624997
kamie <k.me4dev@gmail.com>
2026-09-18 16:22:20 +0900

    feat: Wizard: 필드/메소드에 접근 지정자 추가 및 getter/setter 생성

--- src/main/java/com/survivalcoding/day04_calss_instance/Wizard.java
+++ src/main/java/com/survivalcoding/day04_calss_instance/Wizard.java
@@ -1,10 +1,28 @@
 package com.survivalcoding.day04_calss_instance;
 
 public class Wizard {
-    String name;
-    int hp;
+    private String name;
+    private int hp;
     
-    void heal(Hero hero) {
+    // getter/setter
+    public String getName() {
+        return name;
+    }
+    
+    public void setName(String name) {
+        this.name = name;
+    }
+    
+    public int getHp() {
+        return hp;
+    }
+    
+    public void setHp(int hp) {
+        this.hp = hp;
+    }
+    
+    // method
+    public void heal(Hero hero) {
         hero.setHp(hero.getHp() + 10);
         System.out.printf("%s의 HP 10을 회복했다!%n", hero.getName());
     }
