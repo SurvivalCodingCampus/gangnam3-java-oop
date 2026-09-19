d3dcf2138db392a393ea39df24be40a2d1fdb9f5
Jungi Kim <a43361904@gmail.com>
2026-09-19 14:30:56 +0900

    feat: Wand 클래스 추가 및 Wizard 캡슐화, 유효성 검사 및 단위 테스트 구현

--- src/main/java/com/survivalcoding/day01_class_instance/Wizard.java
+++ src/main/java/com/survivalcoding/day01_class_instance/Wizard.java
@@ -3,21 +3,78 @@ package com.survivalcoding.day01_class_instance;
 public class Wizard {
 
     // field
-    public String name;
-    public int hp;
+    private String name;
+    private int hp;
+    private int mp;
+    private Wand wand;
 
     // constructor
-    public Wizard() {
-
+    public Wizard(String name, int hp, int mp, Wand wand) {
+        this.hp = hp;
+        this.mp = mp;
+        this.name = name;
+        this.wand = wand;
     }
 
-    public Wizard(String name, int hp) {
+    // method
+    public void heal(Hero hero) {
+        int basePoint = 10;
+        int recovPoint = (int) (basePoint * this.wand.getPower());
+
+        hero.setHp(hero.getHp() + recovPoint);
+    }
+
+    // getter
+    public String getName() {
+        return name;
+    }
+
+    public int getHp() {
+        return hp;
+    }
+
+    public int getMp() {
+        return mp;
+    }
+
+    public Wand getWand() {
+        return wand;
+    }
+
+    // setter
+    public void setName(String name) {
+        if (name == null) {
+            throw new IllegalArgumentException("name(이름)은 null이 아니어야 합니다");
+        }
+
+        if (name.length() < 3) {
+            throw new IllegalArgumentException("name(이름)은 3글자 이상이어야 합니다");
+        }
+
         this.name = name;
+    }
+
+    public void setHp(int hp) {
+        if (hp < 0) {
+            throw new IllegalArgumentException("hp(체력)는 0 이상이어야 합니다");
+        }
+
         this.hp = hp;
     }
 
-    public void heal(Hero hero) {
-        hero.hp += 10;
-        System.out.println(hero.name + "의 HP를 10 회복했다!");
+    public void setMp(int mp) {
+        if (mp < 0) {
+            throw new IllegalArgumentException("mp(마력)는 0 이상이어야 합니다");
+        }
+
+        this.mp = mp;
+    }
+
+    public void setWand(Wand wand) {
+        if (wand == null) {
+            throw new IllegalArgumentException("wand(지팡이)는 null이 아니어야 합니다");
+        }
+
+        this.wand = wand;
     }
 }
--- src/test/java/com/survivalcoding/day01_class_instance/WizardTest.java
+++ src/test/java/com/survivalcoding/day01_class_instance/WizardTest.java
@@ -1,23 +1,132 @@
 package com.survivalcoding.day01_class_instance;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
 
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
+@DisplayName("Wizard 클래스 테스트")
 class WizardTest {
 
+    Wizard wizard;
+
+    @BeforeEach
+    void setUp() {
+        Wand wand = new Wand("나무", 10.0);
+        wizard = new Wizard("위저드", 50, 10, wand);
+    }
+
     @Test
-    @DisplayName("heal을 하면 hp를 10 회복해야 한다")
-    void heal() {
+    @DisplayName("heal을 하면 `power * point`만큼 hp를 회복해야 한다")
+    void heal_shouldBeHp_powerTimesTen() {
         // given
-        final Wizard wizard = new Wizard("마법사", 100);
-        final Hero hero = new Hero("히어로", 10);
+        Hero hero = new Hero("히어로", 10);
+        double power = wizard.getWand().getPower();
+        int point = 10;
+        int expected = (int) (power * point) + hero.getHp();
 
         // when
         wizard.heal(hero);
 
         // then
-        assertEquals(20, hero.hp);
+        assertEquals(expected, hero.getHp());
+    }
+
+    @Test
+    @DisplayName("설정할 이름은 null이 아니어야 한다")
+    void setName_shouldNotBeNull() {
+        // given
+        String name = null;
+        String errorMessage = "name(이름)은 null이 아니어야 합니다";
+
+        // when
+        IllegalArgumentException error = assertThrows(IllegalArgumentException.class, () -> wizard.setName(name));
+
+        // then
+        assertEquals(errorMessage, error.getMessage());
+    }
+
+    @Test
+    @DisplayName("설정할 이름은 최소 3글자 이상을 입력해야 한다")
+    void setName_shouldBeLetters_atLeastThree() {
+        // given
+        String name = "위저";
+        String errorMessage = "name(이름)은 3글자 이상이어야 합니다";
+
+        // when
+        IllegalArgumentException error = assertThrows(IllegalArgumentException.class, () -> wizard.setName(name));
+
+        // then
+        assertEquals(errorMessage, error.getMessage());
+    }
+
+    @Test
+    @DisplayName("hp는 0 이상이어야 한다")
+    void setHp_shouldBeHp_moreThanZero() {
+        // given
+        int hp = -1;
+        String errorMessage = "hp(체력)는 0 이상이어야 합니다";
+
+        // when
+        IllegalArgumentException error = assertThrows(IllegalArgumentException.class, () -> wizard.setHp(hp));
+
+        // then
+        assertEquals(errorMessage, error.getMessage());
+    }
+
+    @Test
+    @DisplayName("hp가 0이면 정상적으로 실행되어야 한다")
+    void setHp_shouldBeExecutedSuccessfully() {
+        // given
+        int hp = 0;
+
+        // when
+        wizard.setHp(hp);
+
+        // then
+        assertEquals(hp, wizard.getHp());
+    }
+
+    @Test
+    @DisplayName("mp는 0 이상이어야 한다")
+    void setMp_shouldBeMp_moreThanZero() {
+        // given
+        int mp = -1;
+        String errorMessage = "mp(마력)는 0 이상이어야 합니다";
+
+        // when
+        IllegalArgumentException error = assertThrows(IllegalArgumentException.class, () -> wizard.setMp(mp));
+
+        // then
+        assertEquals(errorMessage, error.getMessage());
+    }
+
+    @Test
+    @DisplayName("mp가 0이면 정상적으로 실행되어야 한다")
+    void setMp_shouldBeExecutedSuccessfully() {
+        // given
+        int mp = 0;
+
+        // when
+        wizard.setMp(mp);
+
+        // then
+        assertEquals(mp, wizard.getMp());
+    }
+
+    @Test
+    @DisplayName("설정할 지팡이는 null이 아니어야 한다")
+    void setWand_shouldNotBeNull() {
+        // given
+        Wand wand = null;
+        String errorMessage = "wand(지팡이)는 null이 아니어야 합니다";
+
+        // when
+        IllegalArgumentException error = assertThrows(IllegalArgumentException.class, () -> wizard.setWand(wand));
+
+        // then
+        assertEquals(errorMessage, error.getMessage());
     }
 }
