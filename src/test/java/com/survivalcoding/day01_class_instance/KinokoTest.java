7d78aef18b7adafa7c086a7040e9b11de8f5bcc8
Jungi Kim <a43361904@gmail.com>
2026-09-25 11:04:25 +0900

    test: Kinoko 사망 메시지 출력 시점 테스트 추가

--- src/test/java/com/survivalcoding/day01_class_instance/KinokoTest.java
+++ src/test/java/com/survivalcoding/day01_class_instance/KinokoTest.java
@@ -2,6 +2,10 @@ package com.survivalcoding.day01_class_instance;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
+import java.io.ByteArrayOutputStream;
+import java.io.PrintStream;
+
+import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
@@ -9,11 +13,26 @@ import org.junit.jupiter.api.Test;
 @DisplayName("Kinoko 클래스 테스트")
 class KinokoTest {
 
+    private static final String DIE_MESSAGE = "버섯는 죽었다";
+
+    private final PrintStream originalOut = System.out;
+    private final ByteArrayOutputStream output = new ByteArrayOutputStream();
+
     Kinoko kinoko;
 
     @BeforeEach
     void setUp() {
         kinoko = new Kinoko("버섯", 50);
+        System.setOut(new PrintStream(output));
+    }
+
+    @AfterEach
+    void tearDown() {
+        System.setOut(originalOut);
+    }
+
+    private int countDieMessage() {
+        return output.toString().split(DIE_MESSAGE, -1).length - 1;
     }
 
     @Test
@@ -70,5 +89,54 @@ class KinokoTest {
 
         // then
         assertEquals(expected, negative.getHp());
+    }
+
+    @Test
+    @DisplayName("피해로 hp가 양수에서 0이 되면 사망 메시지가 한 번 출력되어야 한다")
+    void takeDamage_shouldPrintDieMessageOnce_whenHpBecomesZero() {
+        // given
+        int damage = 100;
+        int expectedHp = 0;
+        int expectedCount = 1;
+
+        // when
+        kinoko.takeDamage(damage);
+
+        // then
+        assertEquals(expectedHp, kinoko.getHp());
+        assertEquals(expectedCount, countDieMessage());
+    }
+
+    @Test
+    @DisplayName("hp가 이미 0일 때 추가 피해를 받으면 사망 메시지가 다시 출력되지 않아야 한다")
+    void takeDamage_shouldNotPrintDieMessageAgain_whenHpIsAlreadyZero() {
+        // given
+        kinoko.takeDamage(100);
+        int damage = 10;
+        int expectedHp = 0;
+        int expectedCount = 1;
+
+        // when
+        kinoko.takeDamage(damage);
+
+        // then
+        assertEquals(expectedHp, kinoko.getHp());
+        assertEquals(expectedCount, countDieMessage());
+    }
+
+    @Test
+    @DisplayName("setHp로 hp를 0으로 설정하면 사망 메시지가 출력되지 않아야 한다")
+    void setHp_shouldNotPrintDieMessage_whenHpIsSetToZero() {
+        // given
+        int hp = 0;
+        int expectedHp = 0;
+        int expectedCount = 0;
+
+        // when
+        kinoko.setHp(hp);
+
+        // then
+        assertEquals(expectedHp, kinoko.getHp());
+        assertEquals(expectedCount, countDieMessage());
     }
 }
